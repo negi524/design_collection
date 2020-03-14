c921f6f84a6ca6541935766cefd535dc56923a8e
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-03-14 12:25:57 +0900

    ボタンページ追加

---
+++ pages/buttons.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container mt-5">
+    <h2 class="p-2 text-success">Button List</h2>
+    <div class="container my-3">
+      <h3>hogehoge</h3>
+      <div class="row">
+        <div class="col-6">見た目</div>
+        <div class="col-6">ソースコード</div>
+      </div>
+    </div>
+  </div>
+</template>
