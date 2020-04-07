469ef87cce964e7187ffb2edea71a7b33219d0ec
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-04-07 09:16:59 +0900

    新規ページを追加

---
+++ pages/CherryBlossoms.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <global-header :crumbs="crumbs" />
+  </div>
+</template>
+
+<script>
+import GlobalHeader from '~/components/GlobalHeader.vue'
+
+export default {
+  components: {
+    GlobalHeader
+  },
+  data() {
+    return {
+      crumbs: [
+        { text: 'Home', href: '/' },
+        { text: 'CherryBlossoms', href: '/cherryblossoms' }
+      ]
+    }
+  }
+}
+</script>
