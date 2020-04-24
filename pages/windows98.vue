2a4db8c9943c6cc445edd101c344f9ca90f9dd19
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-04-25 01:25:02 +0900

    windows98のレイアウト調整とウィンドウ配置

--- pages/windows98.vue
+++ pages/windows98.vue
@@ -1,28 +1,53 @@
 <template>
-  <div class="window">
-    <div class="title-bar">
-      <div class="title-bar-text">
-        My First VB4 Program
+  <div class="window w-98__container">
+    <div class="row">
+      <div class="col-3">
+        <ul class="tree-view">
+          <li>
+            home
+            <ul>
+              <li><nuxt-link to="/buttons">Button Page</nuxt-link></li>
+              <li><nuxt-link to="/snowfall">Snow Fall</nuxt-link></li>
+              <li><nuxt-link to="/windows98">Windows98</nuxt-link></li>
+            </ul>
+          </li>
+        </ul>
       </div>
-    </div>
-    <div class="window-body">
-      <p>Hello, world!</p>
+      <div class="col-8">
+        <div class="window" style="width: 300px">
+          <div class="title-bar">
+            <div class="title-bar-text">ようこそ</div>
+            <div class="title-bar-controls">
+              <button aria-label="Minimize"></button>
+              <button aria-label="Maximize"></button>
+              <button aria-label="Close"></button>
+            </div>
+          </div>
+          <div class="window-body">
+            <p>懐かしすぎるページ</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-// windows98のモジュールを利用する
-import '98.css'
-
 export default {
   layout: 'onepage'
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+/* このvueコンポーネントのみでwindows98のcssを利用する */
+@import url('98.css');
+
 .window {
-  margin: 32px;
-  width: 250px;
+  font-size: 14px;
+}
+.w-98 {
+  &__container {
+    height: 1000px;
+  }
 }
 </style>
